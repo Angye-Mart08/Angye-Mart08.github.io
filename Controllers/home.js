6ce91147a583e35cc42695bf228f41fa941e3625
Angye-Mart08 <162449711+Angye-Mart08@users.noreply.github.com>
2024-05-07 22:47:19 -0500

    Delete Controllers directory

--- Controllers/home.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('logout-form').addEventListener('submit', function(event) {
-    var confirmLogout = confirm('¿Estás seguro de que deseas cerrar sesión?');
-    if (!confirmLogout) {
-        event.preventDefault();
-    } else {
-        alert('Sesión cerrada');
-    }
-});
-
